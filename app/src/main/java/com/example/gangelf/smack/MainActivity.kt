63ba2663eaaad7bd3d657725e982b8ac8f7a0f32
Alex Lyzhoft <alexlyz@gmail.com>
2018-02-21 19:52:46 -0600

    Imported Assets Removed unused items from layouts converted liner layout to constraint layout

--- app/src/main/java/com/example/gangelf/smack/MainActivity.kt
+++ app/src/main/java/com/example/gangelf/smack/MainActivity.kt
@@ -32,14 +32,4 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
 }
