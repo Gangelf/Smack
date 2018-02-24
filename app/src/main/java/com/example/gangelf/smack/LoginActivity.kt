5015e6930e3153b9e64d8f417c0347023ccd39e2
Alex Lyzhoft <alexlyz@gmail.com>
2018-02-24 13:23:14 -0600

    Create User UI Create User Intent

--- app/src/main/java/com/example/gangelf/smack/LoginActivity.kt
+++ app/src/main/java/com/example/gangelf/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gangelf.smack
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -17,6 +18,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserClicked(view: View) {
+        val createUserIntent = Intent(this, UserCreationActivity::class.java)
+        startActivity(createUserIntent)
 
     }
 }
---
+++ app/src/main/java/com/example/gangelf/smack/UserCreationActivity.kt
@@ -0,0 +1,26 @@
+package com.example.gangelf.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class UserCreationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_creation)
+    }
+
+    fun createGenerateAvatarClicked(view: View) {
+
+    }
+
+    fun createGenBackColorClicked(view: View) {
+
+    }
+
+    fun createUserClicked(view: View) {
+
+    }
+
+}
