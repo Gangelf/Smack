6eef1236b00740d3072fe958a0984820655b5465
Alex Lyzhoft <alexlyz@gmail.com>
2018-02-26 19:23:05 -0600

    Downloading Messages

--- app/src/main/java/com/example/gangelf/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/gangelf/smack/Controller/MainActivity.kt
@@ -111,6 +111,15 @@ class MainActivity : AppCompatActivity() {
     fun updateWithChannel() {
         mainChannelName.text = "${selectedChannel?.name}"
         //Download Messages for channel
+        if(selectedChannel != null){
+            MessageService.getMessages(selectedChannel!!.id) {complete ->
+                if(complete){
+                    for (message in MessageService.messages){
+                        println(message.message)
+                    }
+                }
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -162,14 +171,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val onNewChannel = Emitter.Listener {args ->
-        runOnUiThread {
-            val channelName = args[0] as String
-            val channelDesc = args[1] as String
-            val channelId = args[2] as String
+        if(App.prefs.isLoggedIn){
+            runOnUiThread {
+                val channelName = args[0] as String
+                val channelDesc = args[1] as String
+                val channelId = args[2] as String
 
-            val newChannel = Channel(channelName, channelDesc, channelId)
-            MessageService.channels.add(newChannel)
-            channelAdapter.notifyDataSetChanged()
+                val newChannel = Channel(channelName, channelDesc, channelId)
+                MessageService.channels.add(newChannel)
+                channelAdapter.notifyDataSetChanged()
+            }
         }
     }
 
@@ -186,18 +197,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val onNewMessage = Emitter.Listener { args ->
-        runOnUiThread {
-            val messageBody = args[0] as String
-            val channelId = args[2] as String
-            val userName = args[3] as String
-            val userAvatar = args[4] as String
-            val userAvatarColor = args[5] as String
-            val id = args[6] as String
-            val timeStamp = args[7] as String
+        if(App.prefs.isLoggedIn) {
+            runOnUiThread {
+                val channelId = args[2] as String
+                if(channelId == selectedChannel?.id) {
+                    val messageBody = args[0] as String
+                    val userName = args[3] as String
+                    val userAvatar = args[4] as String
+                    val userAvatarColor = args[5] as String
+                    val id = args[6] as String
+                    val timeStamp = args[7] as String
 
-            val newMessage = Message(messageBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
-            MessageService.messages.add(newMessage)
-            println(newMessage.message)
+                    val newMessage = Message(messageBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+                    MessageService.messages.add(newMessage)
+                }
+            }
         }
     }
 
--- app/src/main/java/com/example/gangelf/smack/Services/MessageService.kt
+++ app/src/main/java/com/example/gangelf/smack/Services/MessageService.kt
@@ -9,6 +9,7 @@ import com.example.gangelf.smack.Controller.App
 import com.example.gangelf.smack.Model.Channel
 import com.example.gangelf.smack.Model.Message
 import com.example.gangelf.smack.Utilities.URL_GET_CHANNELS
+import com.example.gangelf.smack.Utilities.URL_GET_MESSAGES
 import org.json.JSONException
 
 /**
@@ -55,5 +56,57 @@ object MessageService {
             }
         }
         App.prefs.requestQueue.add(channelsRequest)
+    }
+
+    fun getMessages(channelId: String, complete: (Boolean) -> Unit) {
+        val url = "$URL_GET_MESSAGES$channelId"
+
+        val messagesRequest = object : JsonArrayRequest(Method.GET, url, null, Response.Listener { response ->
+
+            clearMessages()
+            try {
+
+                for (x in 0 until response.length()) {
+                    val message = response.getJSONObject(x)
+                    val messageBody = message.getString("messageBody")
+                    val channelId = message.getString("channelId")
+                    val id = message.getString("_id")
+                    val userName = message.getString("userName")
+                    val userAvatar = message.getString("userAvatar")
+                    val userAvatarColor = message.getString("userAvatarColor")
+                    val timeStamp = message.getString("timeStamp")
+
+                    val newMessage = Message(messageBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+                    this.messages.add(newMessage)
+                }
+                complete(true)
+
+            } catch (e: JSONException) {
+                Log.d("ERROR", "EXC:" + e.localizedMessage)
+                complete(false)
+            }
+        }, Response.ErrorListener { error ->
+            Log.d("Error", "Could not receive messages: $error")
+            complete(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "applicaltion/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer ${App.prefs.authToken}")
+                return headers
+            }
+        }
+        App.prefs.requestQueue.add(messagesRequest)
+    }
+
+    fun clearMessages() {
+        messages.clear()
+    }
+
+    fun clearChannels() {
+        channels.clear()
     }
 }
